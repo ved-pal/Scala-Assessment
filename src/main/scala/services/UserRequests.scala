87c580f0b9a0559add16bca416910cf84a592b92
ved <ved@localhost.localdomain>
2019-05-07 17:13:18 +0530

    Story 3

--- src/main/scala/services/UserRequests.scala
+++ src/main/scala/services/UserRequests.scala
@@ -10,16 +10,53 @@ class UserRequests  (private val items : mutable.Map[String,Int], val coins : Li
     items
   }
 
-  def viewSelectedItems(i : String) : Map[String,Int] = {
-    if(!items.contains(i)){
-      val temp : Map[String,Int] = Map("" -> 0)
+  def viewSelectedItems(i : String) : Map[String,Int] = {   //view selected items and thier prices
+    if (!items.contains(i)) {
+      val temp: Map[String, Int] = Map("" -> 0)
       return temp
     }
     else {
       val temp: Map[String, Int] = Map(i -> items(i))
       return temp
     }
-
   }
 
+   //------------------ story 3 funtions-----
+    def buyItem(itemname : String) ( insertedcoins : List[Int] , insertednotes :List[Int]): Map[Int, Boolean] = {
+
+      if(insertedcoins.isEmpty || insertednotes.isEmpty){
+        return Map(0 -> false)
+      }
+
+
+      val amountinserted = insertedcoins.sum + insertednotes.sum   // total amount inserted
+
+      if(items.contains(itemname)){
+        val amountrefund = amountinserted- items(itemname)     //items(itemname) = price of that item
+
+        //val res = ischangeavailable(amountrefund)  // function to check change
+
+        return Map(amountrefund -> true)
+      }
+      else{
+        return Map((insertedcoins.sum + insertednotes.sum) -> false)
+      }
+    }
+
+
+
+// function to check notations for refund
+  def ischangeavailable(amf : Int) : Boolean = {
+    var temp = coins ::: notes
+
+    var a = amf
+    if((coins.sum + notes.sum) >= amf ){
+      while(a >0 )
+      if(notes.max <= amf ){a = a - temp.max; temp = temp.filter(_ < temp.max)}
+      if(coins.max <= amf ){a = a - temp.max; temp = temp.filter(_ < temp.max)}
+    }
+
+    if(a == 0) true
+    else false
+  }
 }
--- src/test/scala/TestingVendingMachine.scala
+++ src/test/scala/TestingVendingMachine.scala
@@ -6,8 +6,8 @@ import scala.collection.mutable
 
 class TestingVendingMachine extends FunSuite{
 
-  val adminuser = new VendingMachine(mutable.Map("Chips" -> 10 , "Nuts" -> 15 , "Candies" -> 5, "Biscuits" -> 20, "Cold drinks" -> 20), List(1,2,5,10), List(10,20,50,100))
-  val user = new UserRequests(adminuser.showItems(), List(1,2,5,10), List(10,20,50,100))
+  val adminuser =  VendingMachine(mutable.Map("Chips" -> 10 , "Nuts" -> 15 , "Candies" -> 5, "Biscuits" -> 20, "Cold drinks" -> 20), List(1,2,5,10), List(10,20,50,100))
+
 
 
 
@@ -24,6 +24,8 @@ class TestingVendingMachine extends FunSuite{
     assert(adminuser.showItems().contains("Cake") == true)
     assert(adminuser.showItems().contains("Hot Chocolate") == true)
   }
+
+  val user = new UserRequests(adminuser.showItems(), adminuser.showCoins(), adminuser.showNotes())
 
   test("Story 1 Acceptance Criteria 2"){
     adminuser.addCoins(50)
@@ -46,5 +48,17 @@ class TestingVendingMachine extends FunSuite{
     assert(user.viewSelectedItems("Cold drinks") == Map("Cold drinks" -> 20))
     assert(user.viewSelectedItems("Chips") == Map("Chips" -> 10))
   }
+
+
+
+  test("Story 3 Acceptance Criteria 1"){
+    assert(user.buyItem("Biscuits")(List(1,2,2,5),List(10)) == Map(0 -> true))
+    assert(user.buyItem("Nuts")(List(1,1,2,2),List(5,5))== Map(1 -> true))
+    assert(user.buyItem("xyz")(List(1,1,2,2),List(5,5))== Map(16 -> false))
+    assert(user.buyItem("Candies")(List(1,1,1,1,1,1,1),List(10))== Map(12 -> true))
+    assert(user.buyItem("xyz")(List(),List())== Map(0 -> false))
+
+  }
+
 
 }
